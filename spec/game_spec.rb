ff6907ead1c61797b34daad24f85a0812cdb5cc0
Irina Lepekhina <lepekhina-irina@yandex.ru>
2022-12-13 15:22:54 -0600

    spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -18,4 +18,26 @@ describe Game do
     expect(game.errors).to eq 0
     expect(game.status).to eq :won
   end
+
+  it 'user losts the game' do
+    # Загадываем слово
+    game = Game.new('слово')
+
+    # Убедимся, что в начале игры статус — игра в процессе
+    expect(game.status).to eq :in_progress
+
+    # Удачно отгадаем несколько букв, симулируя действия игрока
+    game.next_step 'а'
+    game.next_step 'б'
+    game.next_step 'в'
+    game.next_step 'г'
+    game.next_step 'д'
+    game.next_step 'е'
+    game.next_step 'ж'
+    game.next_step 'з'
+
+    # Теперь изучем состояние экземпляра game: количество ошибок и статус
+    expect(game.errors).to eq 7
+    expect(game.status).to eq :lost
+  end
 end
